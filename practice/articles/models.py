d6cfd18e812d19e592acf1e21f93f252a1378582
pjhq2 <pkonu7@gmail.com>
2021-09-02 18:22:13 +0900

    2021-09-02

--- practice/articles/models.py
+++ practice/articles/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.db.models.fields import CharField, DateTimeField, TextField
 
 class Article(models.Model):
-    title = CharField(max_length=28)
-    content = TextField()
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
+    title = models.CharField(max_length=28)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- practice/articles/views.py
+++ practice/articles/views.py
@@ -1,15 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
-
-def index(request):
-    articles = Article.objects.all()
-    context = {
-        'articles': articles
-    }
-    return render(request, 'articles/index.html', context)
-
-
+# CREATE
 def new(request):
     return render(request, 'articles/new.html')
 
@@ -19,11 +11,21 @@ def create(request):
     content = request.POST.get('content')
     created_at = request.POST.get('created_at')
     updated_at = request.POST.get('updated_at')
+    if len(title) > 0 and len(content) > 0:
+        article = Article(title=title, content=content, created_at=created_at, updated_at=updated_at)
+        article.save()
+        return render(request, 'articles/create.html')
+    else:
+        return redirect('articles:new')
 
-    article = Article(title=title, content=content, created_at=created_at, updated_at=updated_at)
-    article.save()
 
-    return render(request, 'articles/create.html')
+# READ
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
 
 
 def detail(request, pk):
@@ -32,3 +34,34 @@ def detail(request, pk):
         'article': article,
     }
     return render(request, 'articles/detail.html', context)
+
+
+# UPDATE
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/edit.html', context)
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.updated_at = request.POST.get('updated_at')
+    if len(article.title) > 0 and len(article.content) > 0:
+        article.save()
+        return redirect('articles:detail', article.pk)
+    else:
+        return redirect('articles:edit', article.pk)
+
+
+# DELETE
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail', article.pk)
